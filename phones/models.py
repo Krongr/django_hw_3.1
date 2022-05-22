54e598a6a5dfdc53edfd0d1693882d4ae667f24e
Artem Nazarov <krongr.study@ya.ru>
2022-05-22 16:10:12 +0300

    Data type for column 'release_date' was changed to 'date'.

--- phones/models.py
+++ phones/models.py
@@ -5,6 +5,6 @@ class Phone(models.Model):
     name = models.CharField(max_length=40)
     image = models.CharField(max_length=200)
     price = models.IntegerField()
-    release_date = models.CharField(max_length=40)
+    release_date = models.DateField()
     lte_exists = models.BooleanField(default=False)
     slug = models.SlugField(unique=True)
